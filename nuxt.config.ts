b96c9f86f4ab5886d4ec5ac587cd28f3aa392ac8
marinzilic7 <marinzilic507@gmail.com>
2025-02-26 18:21:36 +0100

    ispravak nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
-
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
 
-  css:['@/assets/css/main.css'],
-})
+  css: ["@/assets/main.css"],
+});
