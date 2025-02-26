96afee784a76e0a00aa5ad9f4098413d88173d33
marinzilic7 <marinzilic507@gmail.com>
2025-02-26 18:33:53 +0100

    dodavanje bootstrapa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
-  css: ["@/assets/main.scss"],
+  css: ["@/assets/main.scss", "bootstrap/dist/css/bootstrap.min.css"],
+  plugins: [
+    {
+      src: "@/plugins/boostrap.js",
+      mode: "client",
+    },
+  ],
 });
