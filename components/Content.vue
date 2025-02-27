d7be6d11417ee67fd957b151b40b88dc66b29055
marinzilic7 <marinzilic507@gmail.com>
2025-02-27 17:01:55 +0100

    Update

--- components/Content.vue
+++ components/Content.vue
@@ -1,18 +1,39 @@
 <template>
-    <div class="content">
-        <div class="main-content">
-            <p>Free Sample</p>
-            <h1>Powerful Generator and Free <span class="d-block">Figma Sources</span></h1>
-            <p>Startup Framework contains components and complex blocks which can easely be <span class="d-block">integrated into almost any design. All of the components are made in the same style, and <span class="d-block">can easily be integrated into projects, allowin you to create hundreds of solutions.</span></span></p>
-
-        </div>
-
+  <div class="content">
+    <div class="main-content">
+      <p class="text-center">Free Sample</p>
+      <h1 class="text-center mt-3">
+        Powerful Generator and Free <span class="d-block">Figma Sources</span>
+      </h1>
+      <p class="text-center mt-3">
+        Startup Framework contains components and complex blocks which can
+        easely be
+        <span class="d-block"
+          >integrated into almost any design. All of the components are made in
+          the same style, and
+          <span class="d-block"
+            >can easily be integrated into projects, allowin you to create
+            hundreds of solutions.</span
+          ></span
+        >
+      </p>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+@import "@/assets/colors.scss";
+.content {
 
-    .content{
-        background-image: url("@/public/images/content-image.png");
-    }
+  padding: 100px 0;
+  position: relative;
+  z-index: 3;
+
+
+
+  .main-content{
+    position: relative;
+    z-index:3;
+  }
+}
 </style>
