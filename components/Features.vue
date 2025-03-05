4b6756c847deaae76ba6ec2534740b8a632895a7
marinzilic7 <marinzilic507@gmail.com>
2025-03-05 21:41:42 +0100

    update

--- components/Features.vue
+++ components/Features.vue
@@ -1,36 +1,38 @@
 <template>
   <div class="features">
     <div class="features-items d-flex align-items-center position-relative z-3">
-      <div class="img-items flex-grow-1 position-relative" >
-        <NuxtImg src="/images/macbook.png" alt="macbook-img"  />
-        <NuxtImg src="/images/screen.png" class="screen" alt="screen" />
+      <div class="img-items flex-grow-1 position-relative">
+        <img
+          src="/images/macbook.png"
+          alt="macbook-img"
+          class="macbook-img-class"
+        />
+        <img src="/images/screen.png" class="screen" alt="screen" />
       </div>
       <div class="flex-grow-1">
         <h1 class="text-light">We Create Something New</h1>
         <p class="text-light mt-3 fs-5">
-          We have created a new product that will help designers,
-          developers and companies create <span class="d-block"
-          > websites for their startups quickly and easily.
+          We have created a new product that will help designers, developers and
+          companies create
+          <span class="d-block">
+            websites for their startups quickly and easily.
           </span>
-         
         </p>
-        <div class="d-flex gap-5 mt-5">
-          <div>
+        <div class="d-lg-flex gap-5 mt-5">
+          <div class="mt-5">
             <NuxtImg src="/images/feature-icon.png" alt="feature-icon" />
             <p class="text-light mt-3">30 NEW FEATURE PAGES</p>
             <p class="text-light">
-              Startup Framework contains
-             components and  <span class="d-block">complex blocks which can easily.</span>
-              
+              Startup Framework contains components and
+              <span class="d-block">complex blocks which can easily.</span>
             </p>
           </div>
-          <div>
+          <div class="mt-5">
             <i class="fa-regular fa-gem text-light fs-2"></i>
             <p class="text-light mt-3">USEFUL SYMBOL COMPONENTS</p>
             <p class="text-light">
-              Samples will show you the feeling on
-             how to play  <span class="d-block">around using the components.</span>
-              
+              Samples will show you the feeling on how to play
+              <span class="d-block">around using the components.</span>
             </p>
           </div>
         </div>
@@ -62,12 +64,34 @@
     opacity: 0.5;
   }
 
-  .img-items{
-    .screen{
-        position: absolute;
-        left:0;
-        top:5%;
+ 
+
+  .img-items {
+    .screen {
+      position: absolute;
+      left: 0;
+      top: 5%;
     }
+  }
+}
+
+@media screen and (max-width: 878px) {
+  .features {
+    padding: 100px 5vh;
+  }
+}
+
+@media screen and (max-width: 576px) {
+  .features {
+    padding: 50px 5vh;
+  }
+
+  .macbook-img-class,
+  .screen {
+    display: none;
+  }
+  .d-block {
+    display: inline !important;
   }
 }
 </style>
