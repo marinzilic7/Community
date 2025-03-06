6e96c99339dd459bf1fc2cc0aed4b611ef764de3
marinzilic7 <marinzilic507@gmail.com>
2025-03-06 14:00:30 +0100

    rad na respozivnosti

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,32 +1,71 @@
 <template>
-  <div class="container">
-    <div>
-      <div>
+  <div
+    class="container d-flex flex-column align-items-center justify-content-center"
+  >
+    <div class="main-gallery-items">
+      <div class="d-flex justify-content-between align-items-center mt-5">
         <h1>Last Works</h1>
-        <button class="btn btn-primray">View all Works</button>
+        <button class="btn btn-primary">View all Works</button>
       </div>
-      <div>
+      <div class="gallery-items mt-5">
         <div>
-          <img src="@/public/images/photo1.png" alt="" />ž
-          <p>UI KIT</p>
+          <img src="@/public/images/photo1.png" alt="" />
+          <p class="mt-4">UI KIT</p>
           <h2>Mozart Project</h2>
         </div>
         <div>
           <img src="@/public/images/photo2.png" alt="" />
-          <p>Framework</p>
+          <p class="mt-4">Framework</p>
           <h2>Startup Framework 2.0</h2>
         </div>
         <div>
           <img src="@/public/images/photo3.png" alt="" />
-          <p>UI PHOTOS</p>
+          <p class="mt-4">UI PHOTOS</p>
           <h2>Fromo the Sky</h2>
         </div>
         <div>
           <img src="@/public/images/photo4.png" alt="" />
-          <p>Pictures</p>
+          <p class="mt-4">Pictures</p>
           <h2>Air Forces</h2>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+@import "@/assets/colors.scss";
+.container {
+  padding: 100px 0;
+  button {
+    background-color: $button-color;
+    border: none;
+    border-radius: 20px;
+    padding: 10px 20px;
+  }
+  .gallery-items {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 40px;
+  }
+}
+
+@media screen and (max-width: 878px) {
+  .gallery-items {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr) !important;
+    gap: 20px;
+  }
+}
+
+@media screen and (max-width: 576px) {
+  .main-gallery-items {
+    padding: 0 50px;
+  }
+  .gallery-items {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr) !important;
+    gap: 20px;
+  }
+}
+</style>
