00b4e2a62748bf2574be9b6bb7fd3192c2b8888d
marinzilic7 <marinzilic507@gmail.com>
2025-03-06 13:08:16 +0100

    nastavak rada na reviewsu

---
+++ components/Reviews.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="reviews">
+    <div class="reviews-items">
+      <h1 class="text-light">
+        Our Happy Clients
+        <div>
+          <div>
+            <img src="" alt="" />
+            <div>
+              <p>
+                "Get a fully retina ready site when
+                <span class="d-block">ou build with Startup Framework.</span>
+                Websites look sharper and more
+                <span class="d-block">gorgeous on devices with retina</span>
+                display support"
+              </p>
+              <p>-Rayhan Curran</p>
+            </div>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <div>
+              <p>
+                "As a business targeting high net
+                <span class="d-block">worth individuals, we were looking</span>
+                for a slick, cool and mini-malistic
+                <span class="d-block"> design for our websitef </span>
+              </p>
+              <p>- Kayley Frame</p>
+            </div>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <div>
+              <p>
+                "The most impornant part of the
+                <span class="d-block"> Startup Framework is the samples </span>"
+              </p>
+              <p>- Gene Whitfield</p>
+            </div>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <div>
+              <p>
+                I've build my website with Startup
+                <span class="d-block"
+                  >just in one day, and it was ready-
+                  <span class="d-block">to-go</span></span
+                >
+              </p>
+              <p>
+                - Allan Kim
+              </p>
+            </div>
+          </div>
+        </div>
+      </h1>
+    </div>
+  </div>
+</template>
